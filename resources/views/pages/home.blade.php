02097b4c1651481d28f3ba1d4d24291becd6b126
Angie <liya0706s@gmail.com>
2024-04-28 14:29:20 +0800

    initial commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,1 @@
+Home blade page
---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PagesController::class, 'home'])->name('home');
+Route::get('cart', [PagesController::class, 'cart'])->name('cart');
+
