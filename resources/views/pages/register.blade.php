8338163195b546ce3167c83976981fb1635575f4
李亞璇(Angie.Lee)-TW <01440498@sftw.com>
2024-08-21 18:13:40 +0800

    chapter4-11 無法npm run watch還有環境變數要設定

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,4 +1,37 @@
 @extends('layouts.master')
 @section('content')
-    Register
+    <section class="login-page">
+        <div class="login-form-box">
+            <div class="login-title">Register</div>
+            <div class="login-form">
+                <form action="" method="post">
+
+                    <div class="field">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name" placeholder="Angie Lee">
+                    </div>
+
+                    <div class="field">
+                        <label for="email">E-mail</label>
+                        <input type="text" id="email" name="email" placeholder="Angie.Lee@gmail.com">
+                    </div>
+
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input type="text" id="password" name="password" placeholder="*******">
+                    </div>
+
+                    <div class="field">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input type="text" id="password_confirmation" name="password_confirmation" placeholder="*******">
+                    </div>
+
+                    <div class="field">
+                        <button type="submit" class="btn btn-primary btn-black">Register</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </section>
 @endsection
